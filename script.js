f5153f9b447d860c711b5d17db853778ce02243c
Yakubuamidu <yakubuamidu577@gmail.com>
2023-06-24 16:38:45 -0700

    Initial commit

--- script.js
+++ script.js
@@ -6,29 +6,34 @@ canvas.width = 500;
 canvas.height = 800;
 
 class Game {
-    constructor(){
+    constructor(ctx, width, height){
+        this.ctx = ctx;
+        this.width = width;
+        this.height = height;
         this.enemies = [];
         this.#addNewEnemy();
         console.log(this.enemies);
     }
 
     update(){
-
+       this.enemies.forEach(object => object.update());
     }
 
     draw(){
-
+        this.enemies.forEach(object => object.draw());
     }
 
     #addNewEnemy(){
-       this.enemies.push(new Enemy());
+       this.enemies.push(new Enemy(this));
     }
 };
 
 class Enemy {
-    constructor(){
-       this.x = 100;
-       this.y = 100;
+    constructor(game){
+       this.game = game;
+       console.log('Game: ', this.game);
+       this.x = this.game.width;
+       this.y = Math.random() * this.game.height;
        this.width = 100;
        this.height = 100;
     }
@@ -42,7 +47,7 @@ class Enemy {
     }
 }
 
-const game = new Game();
+const game = new Game(ctx, canvas.width, canvas.height);
 
 let lastTime = 1;
 
@@ -50,6 +55,8 @@ function animate(timeStamp){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     const deltaTime = timeStamp - lastTime;
     lastTime = timeStamp;
+    game.update();
+    game.draw();
     // some code
     requestAnimationFrame(animate);
 };
