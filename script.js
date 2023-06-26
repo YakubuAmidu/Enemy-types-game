0fcaf12f0d6f7b5691b011287b2559150af93ad4
Yakubuamidu <yakubuamidu577@gmail.com>
2023-06-26 08:56:50 -0700

    Initial commit

--- script.js
+++ script.js
@@ -47,15 +47,26 @@ class Enemy {
     constructor(game){
        this.game = game;
        this.markedForDeletion = false;
+       this.frameX;
+       this.maxFrame = 5;
+       this.frameInterval = 100;
+       this.frameTimer = 0;
     }
 
     update(deltaTime){
       this.x -= this.vx * deltaTime;
       if(this.x < 0 - this.width) this.markedForDeletion = true;
+      if(this.frameTimer > this.frameInterval){
+        if(this.frameX < this.maxFrame) this.frameX++;
+        else this.frameX = 0;
+        this.frameTimer = 0;
+      } else {
+        this.frameTimer += deltaTime;
+      }
     }
 
     draw(ctx){
-      ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+      ctx.drawImage(this.image, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
 
